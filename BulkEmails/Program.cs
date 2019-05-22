ff8083f7fb0ea59a0f4d2044a3a3d0056207bc89
Alexander Naumkin <mallanka2byrzoomkin@yandex.ru>
2019-05-22 18:49:04 +0300

    spam without threadPool

--- BulkEmails/Program.cs
+++ BulkEmails/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
 
 namespace BulkEmails
 {
@@ -10,6 +12,16 @@ namespace BulkEmails
     {
         static void Main(string[] args)
         {
+            Directory.CreateDirectory("f://spam//");
+            int count = 1;
+            while (count <= 50)
+            {
+                var sw = new StreamWriter($"f://spam//{count}.txt");
+                sw.Write("spam text");
+                sw.Dispose();
+                count++;
+                Thread.Sleep(100);
+            }
         }
     }
 }
